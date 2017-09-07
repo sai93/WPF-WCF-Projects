0ee033c700ef360394c5f7fc17842d4bb1f8e5fe
sai93 <saimanideep1993@gmail.com>
2017-09-07 15:32:06 -0400

    Basic Add Operation using WPF & WCF

---
+++ Basic Add/WcfService1/Client1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceReference1.Service1Client s = new ServiceReference1.Service1Client();
+            int a = int.Parse(textBox.Text);
+            int b = int.Parse(textBox1.Text);
+            textBlock3.Text = s.Add(a,b);
+        }
+
+      
+    }
+    
+}
